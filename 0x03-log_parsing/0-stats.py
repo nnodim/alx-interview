39bd29ff78e5c3365e91b31350bc74b700864453
nnodim <nnodimobinna@gmail.com>
2023-04-14 02:09:33 +0100

    0. Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A python script that reads stdin line by line and computes metrics
+"""
+import sys
+
+total_size = 0
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                '404': 0, '405': 0, '500': 0}
+line_count = 0
+
+
+def print_stats():
+    """
+    A function to print the statistics
+    """
+    global total_size, status_codes
+    print("File size: {}".format(total_size))
+    for status in sorted(status_codes.keys()):
+        print("{}: {}".format(status, status_codes[status]))
+
+
+try:
+    for line in sys.stdin:
+        token_list = line.split()
+
+        if len(token_list) > 4:
+            total_size += int(token_list[-1])
+            status_code = token_list[-2]
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
+
+except Exception as err:
+    pass
+
+finally:
+    print_stats()
