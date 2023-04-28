ada2b16c77401a8033a986c165ead2cfafbe2b51
nnodim <nnodimobinna@gmail.com>
2023-04-28 02:05:42 +0100

    0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,6 +4,52 @@ This program solves the N queens problem using backtracking
 """
 import sys
 
+
+def safe(board, row, col):
+    """
+    Check if a position is safe
+    """
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while i < N and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
+    return True
+
+
+def solve(board, col):
+    """
+    Solve the problem using backtracking
+    """
+    if col == N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        print(solution)
+        return True
+    res = False
+    for i in range(N):
+        if safe(board, i, col):
+            board[i][col] = 1
+            res = solve(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
@@ -16,49 +62,6 @@ if __name__ == '__main__':
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    def safe(board, row, col):
-        """
-        Check if a position is safe
-        """
-        for i in range(col):
-            if board[row][i] == 1:
-                return False
-        i = row
-        j = col
-        while i >= 0 and j >= 0:
-            if board[i][j] == 1:
-                return False
-            i -= 1
-            j -= 1
-        i = row
-        j = col
-        while i < N and j >= 0:
-            if board[i][j] == 1:
-                return False
-            i += 1
-            j -= 1
-        return True
-
-    def solve(board, col):
-        """
-        Solve the problem using backtracking
-        """
-        if col == N:
-            solution = []
-            for i in range(N):
-                for j in range(N):
-                    if board[i][j] == 1:
-                        solution.append([i, j])
-            print(solution)
-            return True
-        res = False
-        for i in range(N):
-            if safe(board, i, col):
-                board[i][col] = 1
-                res = solve(board, col + 1) or res
-                board[i][col] = 0
-        return res
 
     board = [[0 for i in range(N)] for j in range(N)]
 
