bd4eb8cc4b8f201b2a0036953b945900ae2c8036
nnodim <nnodimobinna@gmail.com>
2023-04-28 03:07:06 +0100

    0. N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,64 +5,68 @@ This program solves the N queens problem using backtracking
 import sys
 
 
-def safe(board, row, col):
+def chess_board(n: int):
     """
-    Check if a position is safe
+    N Queens
     """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-    i = row
-    j = col
-    while i < N and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j -= 1
-    return True
+    result = []
 
-
-def solve(board, col):
-    """
-    Solve the problem using backtracking
-    """
-    if col == N:
-        solution = []
-        for i in range(N):
-            for j in range(N):
-                if board[i][j] == 1:
-                    solution.append([i, j])
-        print(solution)
+    def check_board(row, col, col_in_row):
+        """Checks board"""
+        for i in range(row):
+            if row - i == abs(col - col_in_row[i]):
+                return False
         return True
-    res = False
-    for i in range(N):
-        if safe(board, i, col):
-            board[i][col] = 1
-            res = solve(board, col + 1) or res
-            board[i][col] = 0
-    return res
+
+    def save_board(row, cols, col_in_row):
+        """
+        Saves the current state of the board
+        """
+        if row == n:
+            con_result = []
+            for i in range(n):
+                temp_result = []
+                for j in range(n):
+                    if j == col_in_row[i]:
+                        temp_result.append(i)
+                        temp_result.append(col_in_row[i])
+                        con_result.append(temp_result)
+                if len(con_result) == n:
+                    result.append(con_result)
+                    temp_result, con_result = [], []
+
+    def place_queen(row, cols, col_in_row):
+        """
+        Place queen
+        """
+        save_board(row, cols, col_in_row)
+        for col in range(n):
+            if cols[col] == 0 and check_board(row, col, col_in_row):
+                cols[col] = 1
+                col_in_row[row] = col
+                place_queen(row + 1, cols, col_in_row)
+                cols[col] = 0
+    place_queen(0, [0]*n, [0]*n)
+    return result
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
+    # Check if N is an integer
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if N < 4:
+
+    # Check if N is at least 4
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0 for i in range(N)] for j in range(N)]
-
-    solve(board, 0)
+    solution = chess_board(n)
+    for nqueen in solution:
+        print(nqueen)
