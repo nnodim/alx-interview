3f358e505fdc6347cb62b3609fe5b6d63ae9c73e
nnodim <nnodimobinna@gmail.com>
2023-05-12 02:38:22 +0100

    update

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,17 +8,17 @@ def rotate_2d_matrix(matrix):
     '''
     rotates a 2d matrix 90 degrees clockwise
     '''
-    left = 0
+    left = 0 
     right = len(matrix) - 1
 
     while left < right:
-        top = left
+        top = left 
         bottom = right
         for i in range(right - left):
-            topLeft = matrix[top][left + i]
+            top_left = matrix[top][left + i]
             matrix[top][left + i] = matrix[bottom - i][left]
             matrix[bottom - i][left] = matrix[bottom][right - i]
             matrix[bottom][right - i] = matrix[top + i][right]
-            matrix[top + i][right] = topLeft
+            matrix[top + i][right] = top_left
         right -= 1
         left += 1
