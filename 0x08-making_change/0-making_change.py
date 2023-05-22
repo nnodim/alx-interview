0431f66e2775013eada9624be6472f1e89ab80e2
nnodim <nnodimobinna@gmail.com>
2023-05-22 01:23:08 +0100

    0. Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+0x08. Making Change
+"""
+import sys
+
+
+def makeChange(coins, total):
+    """
+    0. Change comes from within
+    """
+    if total <= 0:
+        return 0
+
+    min_num = [sys.maxsize for i in range(total + 1)]
+    min_num[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                min_num[i] = min(
+                    min_num[i], 1 + min_num[i - coin])
+
+    if min_num[total] == sys.maxsize:
+        return -1
+
+    return min_num[total]
