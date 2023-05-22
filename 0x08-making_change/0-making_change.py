7bf2186fa4c8acc57ac37d12035607a13328af7b
nnodim <nnodimobinna@gmail.com>
2023-05-22 01:45:06 +0100

    0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,18 +5,18 @@
 
 
 def makeChange(coins, total):
-    """ fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-    # sort the coins in descending order
-    coins.sort(reverse=True)
-    change = 0
+
+    # Create a table to store the minimum number of coins needed for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
     for coin in coins:
-        if total <= 0:
-            break
-        temp = total // coin
-        change += temp
-        total -= (temp * coin)
-    if total != 0:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
         return -1
-    return change
+
+    return dp[total]
