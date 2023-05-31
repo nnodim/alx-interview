c906fac75cbc6375d069049e787c1a506766bd7c
nnodim <nnodimobinna@gmail.com>
2023-06-01 00:14:32 +0100

    0. Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,48 +6,63 @@
 
 def isWinner(x, nums):
     """
-    Determines the winner of a game based on a list of numbers.
+    Determines the winner between Maria and Ben
+    based on the results of playing a game.
     """
-    def is_prime(n):
-        """
-        Determines whether a number is prime or not.
-        """
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5)+1):
+    winnerCounter = {'Maria': 0, 'Ben': 0}
+
+    for n in nums:
+        roundWinner = isRoundWinner(n)
+        if roundWinner is not None:
+            winnerCounter[roundWinner] += 1
+
+    if winnerCounter['Maria'] > winnerCounter['Ben']:
+        return 'Maria'
+    elif winnerCounter['Ben'] > winnerCounter['Maria']:
+        return 'Ben'
+    else:
+        return None
+
+
+def isRoundWinner(n):
+    """
+    Determines the winner of a game where
+    two players take turns removing numbers from a list.
+    """
+    numbers = list(range(1, n + 1))
+    players = ['Maria', 'Ben']
+
+    for i in range(n):
+        currentPlayer = players[i % 2]
+        selectedIdxs = []
+        prime = -1
+
+        for idx, num in enumerate(numbers):
+            if prime != -1:
+                if num % prime == 0:
+                    selectedIdxs.append(idx)
+            else:
+                if isPrime(num):
+                    selectedIdxs.append(idx)
+                    prime = num
+
+        if prime == -1:
+            return players[1] if currentPlayer == players[0] else players[0]
+        else:
+            for idx, val in enumerate(selectedIdxs):
+                del numbers[val - idx]
+
+    return None
+
+
+def isPrime(n):
+    """
+    Determines if a given integer is prime.
+    """
+    if n < 2 or (n % 2 == 0 and n > 2):
+        return False
+    else:
+        for i in range(3, int(n ** 0.5) + 1, 2):
             if n % i == 0:
                 return False
         return True
-
-    def remove_multiples(lst, num):
-        """
-        Removes all multiples of a given number from a list.
-        """
-        return [x for x in lst if x % num != 0]
-
-    def play_game(lst):
-        """
-        This function takes a list of integers 'lst' as input
-        and plays a game between Maria and Ben.
-        """
-        maria_turn = True
-        while lst:
-            prime_nums = [x for x in lst if is_prime(x)]
-            if not prime_nums:
-                break
-            num = max(prime_nums)
-            lst = remove_multiples(lst, num)
-            maria_turn = not maria_turn
-        return "Maria" if not maria_turn else "Ben"
-
-    winners = []
-    for i in range(x):
-        lst = list(range(1, nums[i]+1))
-        winners.append(play_game(lst))
-
-    if winners.count("Maria") > winners.count("Ben"):
-        return "Maria"
-    elif winners.count("Maria") < winners.count("Ben"):
-        return "Ben"
-    else:
-        return None
